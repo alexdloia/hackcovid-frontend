10e9b9cfb98fc05fe51d4ef621b6259a9baf47b9
Daniel Rebelsky <drebelsky@gmail.com>
2020-05-23 14:12:41 -0500

    Enabled sticky header

--- src/header.js
+++ src/header.js
@@ -2,16 +2,52 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './Header.css';
 
-export default function Header(props) {
-  const home = props.selected === "Home";
-  return (
-    <div className={home ? "home-header" : "header"}>
-      <span className="header-logo"><span>hack</span><span>COVID</span></span>
-      <nav className="header-links">
-        <Link to="/" className={home ? "selected" : ""} >Home</Link>
-        <Link to="/projects" className={props.selected === "Projects" ? "selected" : ""} >Projects</Link>
-        <Link to="/post" className={props.selected === "Post" ? "selected" : ""}>Post a Job</Link>
-      </nav>
-    </div>
-  );
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {fixed: false, scroll: this.scroll.bind(this)};
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.state.scroll);
+    this.state.scroll();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.state.scroll);
+  }
+
+  scroll() {
+    const header = document.getElementById("header");
+    let fixed = false;
+    if (this.props.selected === "Home") {
+      fixed = window.scrollY > header.clientHeight;
+    } else {
+      fixed = window.scrollY > header.offsetTop;
+    }
+    if (fixed) {
+      if (!this.state.fixed) {
+      document.getElementById("header-container").style.height = header.clientHeight + "px";
+        this.setState(Object.assign({}, this.state, {fixed: true}));
+      }
+    } else {
+      this.setState(Object.assign({}, this.state, {fixed: false}));
+    }
+  }
+
+  render() {
+    const home = this.props.selected === "Home";
+    return (
+      <div id="header-container">
+        <div className={this.state.fixed ? "header fixed" : home ? "home-header" : "header"} id="header">
+          <span className="header-logo"><span>hack</span><span>COVID</span></span>
+          <nav className="header-links">
+            <Link to="/" className={home ? "selected" : ""} >Home</Link>
+            <Link to="/projects" className={this.props.selected === "Projects" ? "selected" : ""} >Projects</Link>
+            <Link to="/post" className={this.props.selected === "Post" ? "selected" : ""}>Post a Job</Link>
+          </nav>
+        </div>
+      </div>
+    );
+  }
 }
