f29226ee451807a48781623b61b4048d39a14234
Daniel Rebelsky <drebelsky@gmail.com>
2020-05-23 15:37:41 -0500

    Changed en dashes to em dashes

--- src/Home.js
+++ src/Home.js
@@ -22,7 +22,7 @@ export default function Home(props) {
         <div className="content">
           <h2>What we do</h2>
           <strong>One of the biggest bottlenecks in innovation and solution-finding is the difficulty in sourcing talented people to support these efforts. </strong>
-          <p>Our mission is to connect teams across the globe who are working tirelessly to tackle the COVID-19 pandemic – whether that be by developing a new cost-effective design for a ventilator or a more efficient solution for contact tracing – with talented individuals who want to play an active role in this fight.</p>
+          <p>Our mission is to connect teams across the globe who are working tirelessly to tackle the COVID-19 pandemic—whether that be by developing a new cost-effective design for a ventilator or a more efficient solution for contact tracing—with talented individuals who want to play an active role in this fight.</p>
           <p>Ultimately, we hope to live in a world where this site is no longer needed, thanks to incredible people like yourselves who have dedicated time and effort to a cause greater than any one of us… a COVID-free future.</p>
           <h2>Who we are</h2>
           <p>We are a group of students from Stanford University who want to help in the fight against COVID-19. We’ve experienced firsthand the difficulty of bringing together a team of talented individuals to bring our ideas to fruition. As students ourselves, we’ve also seen internships, jobs, and research positions cancelled, leaving plenty of talented people without opportunities to do something they’re passionate about.</p>
