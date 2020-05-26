ffc98e18e26453d55fc2e1e302c1a24bb746125f
Daniel Rebelsky <drebelsky@gmail.com>
2020-05-26 15:35:34 -0500

    Added close button to contact modal

--- src/Project.js
+++ src/Project.js
@@ -75,6 +75,7 @@ class Modal extends React.Component {
         onClick={() => this.props.exit()}
         onScroll={event => event.stopPropagation()}
       >
+        <div className="modal-close" onClick={() => this.props.exit()}></div>
         <div className="modal-content Post" onClick={event => event.stopPropagation()}>
           <form method="post" id="modal-form">
             <h1>Get in touch with {this.props.team}</h1>
