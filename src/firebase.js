4855023888939fd872cd952ffb31f27be37978b1
Alex Loia <alex@loia.com>
2020-06-28 15:47:53 -0600

    moved API keys to ignored JSON

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import firebaseConfig from "./firebaseConfig"
 
-const firebaseConfig = {
- 	apiKey: "AIzaSyCklA2dpaIykJ2ZpkwvhJITlc5sVhIqOcg",
- 	authDomain: "hackcovid-project.firebaseapp.com",
- 	databaseURL: "https://hackcovid-project.firebaseio.com",
- 	projectId: "hackcovid-project",
-  	storageBucket: "hackcovid-project.appspot.com",
-  	messagingSenderId: "251585414236",
-  	appId: "1:251585414236:web:6282cbdf832b2fa9181299",
-  	measurementId: "G-WPF1G3WTG9"
-};
 firebase.initializeApp(firebaseConfig);
 export default firebase;
